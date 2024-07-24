3a5c2cad8822ac1c08f7a1a446b553ae8ab2ec00
Morgan <morgan.foucaut@greta-formations.fr>
2024-07-24 23:23:11 +0200

    feat: new tailwind theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#151817',
+        secondary: '#1e2020',
+        tertiary: '#2d302f',
+        quaternary: '#8d9693',
+        quinary: '#fdfdfd',
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
