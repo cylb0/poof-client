62fcf7ee808c82f95e9ab0b39b550299292f6836
Morgan <morgan.foucaut@greta-formations.fr>
2024-07-24 23:23:51 +0200

    feat: menu content constant file

---
+++ src/constants/content/menu.ts
@@ -0,0 +1,23 @@
+/**
+ * Top menu content
+ */
+export const menu = {
+    elements: [
+        {
+            title: 'About', 
+            link: '/about',
+        },
+        {
+            title: 'Pricing',
+            link: '/pricing',
+        },
+    ],
+    signIn: {
+        title: 'Sign In',
+        link: '/login',
+    },
+    signUp: {
+        title: 'Sign Up',
+        link: '/register',
+    }
+}
