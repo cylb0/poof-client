1d6af103daa5cfd35ae1247cde52d772e93bd66c
Morgan <morgan.foucaut@greta-formations.fr>
2024-07-24 19:32:14 +0200

    feat: fixed config files, added subpaths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,15 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss()]
+    }
+  },
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@contexts': path.resolve(__dirname, './src/contexts'),
+      '@hooks:': path.resolve(__dirname, './src/hooks'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
